8af723b897c0e593470c370074ba262e592d9530
Abdus Sadik Molla <sadik.only4work@gmail.com>
2026-07-06 21:30:54 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let i=0;
+        let j=numbers.length-1
+        while (i<j){
+            let sum = numbers[i]+numbers[j]
+            if(sum===target) return [i+1,j+1];
+            if(sum>target) j--;
+            if(sum<target) i++;
+        }
+        return []
+    }
+}
